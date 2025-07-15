b6b6d02e85be0ae02f3cc6442ad72a96b50fcbf5
Marwani Wael <99484717+marwaniiwael18@users.noreply.github.com>
2025-07-15 20:07:27 +0100

    Create Day23_ValidWord.js

---
+++ Day23_ValidWord.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var isValid = function(word) {
+    if (word.length < 3) return false;
+
+    const vowels = new Set('aeiouAEIOU');
+    let hasVowel = false;
+    let hasConsonant = false;
+
+    for (let ch of word) {
+        if (!/[a-zA-Z0-9]/.test(ch)) return false;
+
+        if (/[a-zA-Z]/.test(ch)) {
+            if (vowels.has(ch)) {
+                hasVowel = true;
+            } else {
+                hasConsonant = true;
+            }
+        }
+    }
+
+    return hasVowel && hasConsonant;
+};
